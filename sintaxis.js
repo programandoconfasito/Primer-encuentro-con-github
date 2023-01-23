47c6be3aef4f4c042fa8d4429773f1ed20e5f314
Lupinewolf <proyectodeprogramador@gmail.com>
2023-01-23 02:52:28 -0300

    sintaxis y conexión a html, además de botón con horario

---
+++ sintaxis.js
@@ -0,0 +1,17 @@
+var miVariable; // Esta es la instrucción para crear una variable y asignarle un valor, termina en ; como limitador
+let nombre;
+const iva = 1.21; 
+
+//comentario de una sola línea
+/**
+ * Esto es un 
+ * comentario de 
+ * muchas líneas
+ */
+
+/*
+Esto también es un comentario de muchas líneas 
+asdasd
+asdasd
+asdasdasd
+*/
